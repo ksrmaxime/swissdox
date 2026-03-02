2750390a21bc8aafcb827bd3194142c2c99e3f37
ksrmaxime <maxime.kaiser@icloud.com>
2026-03-02 10:33:35 +0100

    debug

--- sbatch_run_all.sh
+++ sbatch_run_all.sh
@@ -42,8 +42,8 @@ python scripts/run_all_pipeline.py \
   --topic_col TOPIC \
   --sentiment_col SENTIMENT \
   --populism_col POPULISM \
-  --batch_size 50 \
-  --max_new_tokens 300 \
+  --batch_size 40 \
+  --max_new_tokens 100 \
   --temperature 0.0
 
 # --- Archive: outputs + prompt/config/sbatch ---
