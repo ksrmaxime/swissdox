d86e4bd3bb245fa93fd6ec008dee1031f3f4ee97
ksrmaxime <maxime.kaiser@icloud.com>
2026-04-05 22:56:53 +0200

    debug

--- sbatch_run_article.sh
+++ sbatch_run_article.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
-#SBATCH --time=04:00:00
+#SBATCH --time=10:00:00
 #SBATCH --output=logs/run_article_swissrelated_%j.out
 #SBATCH --error=logs/run_article_swissrelated_%j.err
 #SBATCH --mail-user=maxime.kaiser@unil.ch
