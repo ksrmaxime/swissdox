4d9fb07f4749e6966f79b8f7b66f218e9897b4e9
ksrmaxime <maxime.kaiser@icloud.com>
2026-09-04 10:52:40 +0200

    debug

--- sbatch_04_classify_stance.sh
+++ sbatch_04_classify_stance.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
-#SBATCH --time=24:00:00
+#SBATCH --time=3-00:00:00
 #SBATCH --output=logs/criticism_array_%A_%a.out
 #SBATCH --error=logs/criticism_array_%A_%a.err
 #SBATCH --mail-user=maxime.kaiser@unil.ch
